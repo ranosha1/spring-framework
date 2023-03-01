fe81cabcf54e854588b5fc67209c2cdd96c3f44c
ranob <ranobekmuratova@gamil.com>
2023-02-28 20:26:30 -0500

    Spring

--- spring-framework/Spring-08-Spring-MVC-Model/src/main/java/controller/StudentController.java
+++ spring-framework/Spring-08-Spring-MVC-Model/src/main/java/controller/StudentController.java
@@ -1,16 +1,37 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
 public class StudentController {
 
-    @RequestMapping("/welcome1")
+    @RequestMapping("/welcome")
     public String homePage(Model model){
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course","MVC");
 
+        String subject="Collections";
+        model.addAttribute("subject",subject);
 
-        return "st/welcome1";
+        //create some random studentId(0-1000) and show it in your UI
+
+        int studentId=new Random().nextInt();
+        model.addAttribute("id",studentId);
+
+        List <Integer> numbers=new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(10);
+        model.addAttribute("numbers",numbers);
+
+
+        return "/student/welcome";
     }
 }
