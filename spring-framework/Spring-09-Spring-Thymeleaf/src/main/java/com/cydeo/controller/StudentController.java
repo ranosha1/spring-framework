327f6590b3ec325d70c5c2b4dbc127a9f32a7e14
ranob <ranobekmuratova@gamil.com>
2023-03-18 13:26:03 -0400

    thymeleaf trac

---
+++ spring-framework/Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,30 @@
+package com.cydeo.controller;
+
+import com.cydeo.bootstrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+
+    //    @RequestMapping(value = "/register",method = RequestMethod.GET)
+    @GetMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+    //    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
+    public String info(){
+        return "student/welcome";
+    }
+
+
+}
