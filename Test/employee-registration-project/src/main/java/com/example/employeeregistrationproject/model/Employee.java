d4bb55b0bf899ca9ed59dd33fa57c3b5cc7483c9
ranob <ranobekmuratova@gamil.com>
2023-03-19 20:19:20 -0400

    employee project

---
+++ Test/employee-registration-project/src/main/java/com/example/employeeregistrationproject/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.employeeregistrationproject.model;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipcode;
+
+}
