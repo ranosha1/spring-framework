f670a228fae443389ec3ee1cc9a2176236c47a28
ranob <ranobekmuratova@gamil.com>
2023-04-22 14:31:34 -0400

    thymeleaf trac

---
+++ spring-framework/Spring-09-Spring-Thymeleaf/src/test/java/com/example/spring09springthymeleaf/Spring09SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring09springthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SpringThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
