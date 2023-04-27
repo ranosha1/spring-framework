a26b4908df10f58796a721687e5c8fe54c4cacb8
ranob <ranobekmuratova@gamil.com>
2023-04-26 21:38:20 -0400

    Cinemalab application property and pom

---
+++ spring-framework/Spring-13-CinemaLab/src/test/java/com/cydeo/spring13cinemalab/Spring13CinemaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring13cinemalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring13CinemaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
